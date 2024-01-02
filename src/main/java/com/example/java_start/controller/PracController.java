193f3df59a18f189e9a4fa1e2a74f33ce7c696e5
Kojaehyeong <jaehyeong1586@gmail.com>
2024-01-02 16:40:42 +0900

    java_study#1

--- src/main/java/com/example/java_start/controller/PracController.java
+++ src/main/java/com/example/java_start/controller/PracController.java
@@ -1,5 +1,15 @@
 package com.example.java_start.controller;
 
-public class PracController {
+import com.example.java_start.practice.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+public class PracController {
+    @GetMapping("/home")
+    public Course getCourses() {
+        Course course = new Course("이것은 제목", "재형", 20);
+        System.out.println("현재 API 실행");
+        return course;
+    }
 }
